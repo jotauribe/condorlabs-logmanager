c6f66e8a56c0f6f1fab6012b93e0bb3da4a80778
danielyepes <danielyepesahumedo@hotmail.com>
2017-12-14 22:27:35 -0500

    fix schemas

--- models/request.model.js
+++ models/request.model.js
@@ -5,6 +5,6 @@ var RequestSchema = new Schema({
     response_time: Number,
     date: Date,
     parameters: String
-});
+},{versionKey: false }); //to not add field "_v" in json
 
 module.exports = mongoose.model('Requests', RequestSchema);
